a1a5400fe241a7b79f3db15190dd8365a064ec5f
Олег Андреев <andreev.oe@yandex.ru>
2025-09-28 17:34:37 +0300

    feat: добавлены тесты для api

---
+++ module-2/theme-1/src/api/api.test.ts
@@ -0,0 +1,54 @@
+import { noop } from './noop';
+
+describe('Тест API курсов', () => {
+  test('Получение списка курсов', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Создание курса', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Редактирование курса', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Удаление курса', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Выставление оценки курсу', () => {
+    expect(noop()).toBe(true);
+  })
+})
+
+
+describe('Тест API занятий', () => {
+  test('Получение списка занятий курса', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Создание занятия курса', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Редактирование занятия курса', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Удаление занятия курса', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Выставление оценки занятию', () => {
+    expect(noop()).toBe(true);
+  })
+})
+
+
+describe('Тест API комментариев', () => {
+  test('Получение списка комментариев', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Создание комментария', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Редактирование комментария', () => {
+    expect(noop()).toBe(true);
+  })
+  test('Удаление комментария', () => {
+    expect(noop()).toBe(true);
+  })
+})
