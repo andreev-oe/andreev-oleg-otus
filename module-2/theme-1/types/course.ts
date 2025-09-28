81393a860d9675581a1b6c2ae0315b17555af330
Олег Андреев <andreev.oe@yandex.ru>
2025-09-28 17:12:30 +0300

    feat: добавлена модель данных

---
+++ module-2/theme-1/types/course.ts
@@ -0,0 +1,26 @@
+type TExtraMaterials = {
+  type: string;
+  content: any;
+}
+
+type TLesson = {
+  id: string;
+  title: string;
+  description: string;
+  commentsIds: string[];
+  content: any;
+}
+
+type TCourse = {
+  id: string;
+  title: string;
+  description: string;
+  inputOutputExamples: TExtraMaterials[];
+  complexityLevel: 'easy' | 'medium' | 'hard';
+  tags: string[];
+  extraMaterials: TExtraMaterials[];
+  lessons: TLesson[];
+  commentsIds: string[];
+  appointedUsersIds: string[];
+  rating: number;
+}
